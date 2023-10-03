c5ef2ccbf4b09163b0313e7ef2516dce76d74c0d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-03 14:01:54 -0300

    refactor: Altera classe main para app modularizada

--- main.go
+++ main.go
@@ -4,25 +4,19 @@ import (
 	"html/template"
 	"net/http"
 
-	"alura/go-webshop-app/database"
-	"alura/go-webshop-app/products"
+	models "alura/go-webshop-app/models/products"
+
+	_ "github.com/lib/pq"
 )
 
 var myTemplate = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := database.GetDBInstance()
-	defer db.Close() // defer será executado por último
-
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := []products.Product{
-		{Name: "Caneta azul", Description: "Azul caneta", Price: 2, Quantity: 100},
-		{"Tênis", "Air Jordan", 2600, 10},
-	}
-
-	myTemplate.ExecuteTemplate(w, "Index", products)
+	allProducts := models.FindAll()
+	myTemplate.ExecuteTemplate(w, "Index", allProducts)
 }
