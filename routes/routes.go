d7d40105e58f02f5059aef2154e0cefc84f37323
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-09 08:51:03 -0300

    feat: Adiciona rota de edição/atualização

--- routes/routes.go
+++ routes/routes.go
@@ -11,5 +11,6 @@ func LoadRoutes() {
 	http.HandleFunc("/new", controllers.New)
 	http.HandleFunc("/insert", controllers.Insert)
 	http.HandleFunc("/delete", controllers.Delete)
-	http.HandleFunc("/edit", controllers.Update)
+	http.HandleFunc("/edit", controllers.Edit)
+	http.HandleFunc("/update", controllers.Update)
 }
